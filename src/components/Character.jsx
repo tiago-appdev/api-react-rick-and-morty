69ccfe15197e7ab93e76335c03304ef5453a8fc9
Tiago <tiago.appdev@gmail.com>
2024-11-27 17:20:53 -0300

    change minnor details

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,16 +1,16 @@
-import './Character.css';
+import "./character.css";
 
 export function Character(character) {
   return (
-    <div className='card'>
-      <div className='card2 rounded-[17px]'>
-        <h3 className='p-2 font-bold'>{character.name}</h3>
+    <div className="card">
+      <div className="card2 rounded-[17px]">
+        <h3 className="p-2 font-bold">{character.name}</h3>
         <img
           src={character.image}
           alt={character.name}
-          className='w-fit p-3 mx-auto rounded-3xl'
+          className="w-fit p-3 mx-auto rounded-3xl"
         />
-        <p className='p-2'>{`Origen: ${character.origin.name}`}</p>
+        <p className="p-2">{`Origen: ${character.origin.name}`}</p>
       </div>
     </div>
   );
